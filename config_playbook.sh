925947f0c7be1ede8a70704846d11636639b3573
matanelcompie <matanel@compie.co.il>
2022-01-24 13:56:50 +0200

    .

---
+++ config_playbook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file="config.json"
+sum_repos=$(jq '.values | length' $file)
+for i in $(seq 1 $sum_repos);
+do
+repository=$(jq ".values[$i-1].repository"  $file);
+branch=$(jq ".values[$i-1].branch"  $file);
+dst=$(jq ".values[$i-1].dst"  $file);
+cat <<EOF >> ansible/playbook.yaml
+  - name: Clone $i
+    ansible.builtin.git:
+      repo: $repository
+      dest: $dst
+      accept_hostkey: yes
+      key_file: /home/ubuntu/.ssh/github
+      version: $branch
+EOF
+done
